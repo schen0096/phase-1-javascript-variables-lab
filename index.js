284cb32dc3d2e70d28257038fcacd6eefea814cc
steven chen <schen0096@gmail.com>
2021-06-28 11:15:18 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+// let companyName = 'Scuber';
+
+const companyName = 'Scuber';
+
+// const mostProfitableNeighborhood = 'Chelsea';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+// const companyCeo = 'Susan Smith';
+
+let companyCeo = 'Susan Smith';
