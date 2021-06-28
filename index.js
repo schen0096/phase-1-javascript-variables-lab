223a7eb0a0ecef0670c5a39c2a9e40d83331794f
steven chen <schen0096@gmail.com>
2021-06-28 11:14:41 -0400

    commit tree bab32dd65f483d33b0df430be69d2390410badc4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// let companyName = 'Scuber';
+
+const companyName = 'Scuber';
+
+// const mostProfitableNeighborhood = 'Chelsea';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+// const companyCeo = 'Susan Smith';
+
+let companyCeo = 'Susan Smith';
